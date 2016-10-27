24c1a03f1d4a21f6d7ce0c652bdbfd7ae4d58417
Jim Crowley <jamescro@microsoft.com>
2016-10-27 14:33:18 -0700

    refactoring for v2 auth and Flask

--- connect/graph_service.py
+++ connect/graph_service.py
@@ -1,4 +1,5 @@
-# Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+# Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+# See LICENSE in the project root for license information.
 
 import requests
 import uuid
@@ -49,39 +50,10 @@ def call_sendMail_endpoint(access_token, alias, emailAddress):
 		'SaveToSentItems': 'true'
 	}   
 
-  response = requests.post(url = send_mail_url, headers = headers, data = json.dumps(email), params = None)
+  response = requests.post(url = send_mail_url, headers = headers, data = json.dumps(email), verify=False, params = None)
 
 	# Check if the response is 202 (success) or not (failure).
   if (response.status_code == requests.codes.accepted):
     return response.status_code
   else:
     return "{0}: {1}".format(response.status_code, response.text)
-
-#######################################################################
-#  
-# O365-Python-Microsoft-Graph-Connect, https://github.com/OfficeDev/O365-Python-Microsoft-Graph-Connect
-#
-# Copyright (c) Microsoft Corporation
-# All rights reserved.
-#
-# MIT License:
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-#######################################################################
